7d06110e4731eb2b08115a2518e545d5d44ac07f
Justin Britten <justin@compulsivoco.com>
2009-03-04 13:49:31 +0800

    Re-org interests, experts, and speakers.
    Signed-off-by: Austin On Rails <github-austinonrails@damon.otherinbox.com>

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,10 +1,14 @@
 class Topic < ActiveRecord::Base
   has_many :member_interests
-  has_many :enthusiasts, :through => :member_interests, :source => :member
-  has_many :topic_speakers
-  has_many :speakers, :through => :topic_speakers, :source => :member
+  has_many :enthusiasts, :through => :member_interests, :source => :member, :conditions => {'member_interests.is_interested' => true}
+  has_many :experts, :through => :member_interests, :source => :member, :conditions => {'member_interests.is_expert' => true}
+  has_many :speakers, :through => :member_interests, :source => :member, :conditions => {'member_interests.will_speak' => true}
 
   validates_presence_of :name
 
   attr_accessible :name
+  
+  def member_interest(member)
+    member_interests.find_by_member_id(member.id)
+  end
 end
