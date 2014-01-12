62b793e489f68a00ae342fd0465fddcd06dab986
rubygeek <nola@rubygeek.com>
2014-01-11 19:33:07 -0600

    fix typo in spec, doh.

--- spec/routing/member_interests_spec.rb
+++ spec/routing/member_interests_spec.rb
@@ -2,9 +2,9 @@ require "spec_helper"
 
 describe MemberInterestsController do 
   it "should get a list of interests" do
-    expect(get: "/topics/1/interests").to route_to(controller: "MemberInterests", action: "index", topic_id: "1")
+    expect(get: "/topics/1/interests").to route_to(controller: "member_interests", action: "index", topic_id: "1")
   end 	
   it "should have routes for updating interests" do
-  	expect(post: "/topics/1/interests").to route_to(controller: "MemberInterests", action: "create", topic_id: "1")
+  	expect(post: "/topics/1/interests").to route_to(controller: "member_interests", action: "create", topic_id: "1")
   end  	
 end
