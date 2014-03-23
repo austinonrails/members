a88fa015597bea7f703b1d127b14cdb144f9a0fc
rubygeek <nola@rubygeek.com>
2014-03-22 22:18:10 -0500

    updated for asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -46,6 +46,7 @@ module Members
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += %w( *.css *.js ) 
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
